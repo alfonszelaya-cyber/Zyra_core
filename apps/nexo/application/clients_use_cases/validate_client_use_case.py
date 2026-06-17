a52f1ba97775843bf8e73f83cb4a4af8fc1ee896
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-06-17 07:05:02 -0700

    Update validate_client_use_case.py

--- apps/nexo/application/clients_use_cases/validate_client_use_case.py
+++ apps/nexo/application/clients_use_cases/validate_client_use_case.py
@@ -1,1 +1,21 @@
+from apps.nexo.domain.clients.client_validation_engine import (
+    ClientValidationEngine,
+)
 
+
+class ValidateClientUseCase:
+
+    def __init__(
+        self,
+        validation_engine: ClientValidationEngine,
+    ):
+        self._validation_engine = validation_engine
+
+    def execute(
+        self,
+        client_data: dict,
+    ) -> bool:
+
+        return self._validation_engine.validate(
+            client_data=client_data
+        )
