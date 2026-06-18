e9376c0a127e299197826f0810af8e0f952e9848
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-06-18 00:04:18 -0700

    Update client_validation_engine.py

--- apps/nexo/domain/clients/client_validation_engine.py
+++ apps/nexo/domain/clients/client_validation_engine.py
@@ -1,1 +1,18 @@
+class ClientValidationEngine:
 
+    def validate(
+        self,
+        client_data: dict,
+    ) -> bool:
+
+        required_fields = [
+            "name",
+            "document",
+            "email",
+        ]
+
+        return all(
+            field in client_data
+            and client_data[field]
+            for field in required_fields
+        )
