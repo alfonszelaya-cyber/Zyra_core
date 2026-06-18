7a50e4ceb770bd262d1d05f22ced9b59025e3a8c
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-06-17 23:09:37 -0700

    Update validate_operation_use_case.py

--- apps/nexo/application/operations_use_cases/validate_operation_use_case.py
+++ apps/nexo/application/operations_use_cases/validate_operation_use_case.py
@@ -1,1 +1,35 @@
+from apps.nexo.domain.operations.operations_validation_engine import (
+    OperationsValidationEngine,
+)
 
+
+class ValidateOperationUseCase:
+
+    def __init__(
+        self,
+        operations_validation_engine: (
+            OperationsValidationEngine
+        ),
+    ):
+        self._operations_validation_engine = (
+            operations_validation_engine
+        )
+
+    def execute(
+        self,
+        operation_data: dict,
+    ) -> dict:
+
+        validation = (
+            self._operations_validation_engine.validate(
+                operation_data=operation_data,
+            )
+        )
+
+        return {
+            "valid": validation.get(
+                "valid",
+                False,
+            ),
+            "details": validation,
+        }
