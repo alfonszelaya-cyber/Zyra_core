66fbf8eea26b2c48293bf7b786f44a340f2eae06
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-06-17 23:19:57 -0700

    Update calculate_risk_reward_use_case.py

--- apps/nexo/application/radar_vip_use_cases/calculate_risk_reward_use_case.py
+++ apps/nexo/application/radar_vip_use_cases/calculate_risk_reward_use_case.py
@@ -1,1 +1,30 @@
+from apps.nexo.domain.radar_vip.risk_reward_engine import (
+    RiskRewardEngine,
+)
 
+
+class CalculateRiskRewardUseCase:
+
+    def __init__(
+        self,
+        risk_reward_engine: RiskRewardEngine,
+    ):
+        self._risk_reward_engine = (
+            risk_reward_engine
+        )
+
+    def execute(
+        self,
+        operation_data: dict,
+    ) -> dict:
+
+        calculation = (
+            self._risk_reward_engine.calculate(
+                operation_data=operation_data,
+            )
+        )
+
+        return {
+            "risk_reward": calculation,
+            "status": "CALCULATED",
+        }
