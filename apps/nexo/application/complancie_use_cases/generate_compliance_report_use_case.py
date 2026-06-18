aa8f4dc9fa8b68725fea917f47cfcdde7bf6f926
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-06-17 21:54:22 -0700

    Update generate_compliance_report_use_case.py

--- apps/nexo/application/complancie_use_cases/generate_compliance_report_use_case.py
+++ apps/nexo/application/complancie_use_cases/generate_compliance_report_use_case.py
@@ -1,1 +1,30 @@
+from apps.nexo.domain.compliance.compliance_registry import (
+    ComplianceRegistry,
+)
 
+
+class GenerateComplianceReportUseCase:
+
+    def __init__(
+        self,
+        compliance_registry: ComplianceRegistry,
+    ):
+        self._compliance_registry = compliance_registry
+
+    def execute(
+        self,
+        company_id: str,
+        period_id: str,
+    ) -> dict:
+
+        events = self._compliance_registry.get_by_period(
+            company_id=company_id,
+            period_id=period_id,
+        )
+
+        return {
+            "company_id": company_id,
+            "period_id": period_id,
+            "events": events,
+            "total_events": len(events),
+        }
